748faac8a2663261d32e3984f31151e24adaa26e
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-02-10 16:30:10 +0100

    init

---
+++ src/test/java/com/rafalcendrowski/AccountApplication/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafalcendrowski.AccountApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
