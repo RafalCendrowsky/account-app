fd9a129f7bd9fd4fffe1652e5b68de4d30f98166
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-02-10 19:55:43 +0100

    Fixed from NonNull to NotNull, added custom Exception

--- src/main/java/com/rafalcendrowski/AccountApplication/AuthController.java
+++ src/main/java/com/rafalcendrowski/AccountApplication/AuthController.java
@@ -1,53 +1,55 @@
 package com.rafalcendrowski.AccountApplication;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.LinkedHashMap;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Map;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid email")
+class InvalidEmailException extends RuntimeException {
+    public InvalidEmailException() {
+        super();
+    }
+}
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    @ExceptionHandler
-        @ResponseStatus(HttpStatus.BAD_REQUEST)
-        public ResponseEntity<String> handleException(NullPointerException exception) {
-        return ResponseEntity.badRequest().build();
-    }
-
     @PostMapping("/signup")
-    public ResponseEntity<Map<String, String>> addAccount (@Valid @RequestBody Account account) {
+    public ResponseEntity<Map<String, String>> addAccount(@Valid @RequestBody Account account) {
         String[] email = account.getEmail().split("@");
         String domain = email.length == 2 ? email[1] : "";
-        if (!account.getPassword().isEmpty() && domain.equals("acme.com")) {
+        if (domain.equals("acme.com")) {
             return new ResponseEntity<>(
-                    new LinkedHashMap<String, String>(Map.of("name", account.getName(),
-                            "lastname", account.getLastname(), "email", account.getEmail())),
-                    HttpStatus.ACCEPTED);
+                    Map.of("name", account.getName(), "lastname", account.getLastname(),
+                            "email", account.getEmail()), HttpStatus.ACCEPTED);
         } else {
-            return ResponseEntity.badRequest().build();
+            throw new InvalidEmailException();
         }
     }
 }
 
 
 class Account {
-    @NonNull
+    @NotNull
+    @Size(min=1)
     private String name;
-    @NonNull
+    @NotNull
+    @Size(min=1)
     private String lastname;
-    @NonNull
+    @NotNull
+    @Size(min=1)
     private String email;
-    @NonNull
+    @NotNull
+    @Size(min=1)
     private String password;
 
-    public Account(@NonNull String name, @NonNull String lastname,
-                   @NonNull String email, @NonNull String password) {
+    public Account(String name, String lastname, String email, String password) {
         this.name = name;
         this.lastname = lastname;
         this.email = email;
