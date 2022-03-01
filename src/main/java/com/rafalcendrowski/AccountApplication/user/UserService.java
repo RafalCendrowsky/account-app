54ee44353c31b16622c0f459741459886e193665
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-01 17:28:29 +0100

    split user service into interface and implementation

--- src/main/java/com/rafalcendrowski/AccountApplication/user/UserService.java
+++ src/main/java/com/rafalcendrowski/AccountApplication/user/UserService.java
@@ -1,56 +1,16 @@
 package com.rafalcendrowski.AccountApplication.user;
 
-import com.rafalcendrowski.AccountApplication.payment.Payment;
-import com.rafalcendrowski.AccountApplication.user.User;
-import com.rafalcendrowski.AccountApplication.user.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.Locale;
 
-@Service
-@RequiredArgsConstructor
-public class UserService implements UserDetailsService {
+public interface UserService {
 
-    private final UserRepository userRepository;
+    public User loadByUsername(String username);
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username.toLowerCase(Locale.ROOT));
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        } else {
-            return user;
-        }
-    }
+    public User saveUser(User user);
 
-    public User loadByUsername(String username) {
-        User user = userRepository.findByUsername(username.toLowerCase(Locale.ROOT));
-        if (user == null) {
-            throw new IllegalArgumentException("User not found");
-        } else {
-            return user;
-        }
-    }
+    public void deleteUser(User user);
 
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
+    public List<User> loadAllUsers();
 
-    public void deleteUser(User user) {
-        userRepository.delete(user);
-    }
-
-    public List<User> loadAllUsers() {
-        return userRepository.findAll();
-    }
-
-    public boolean hasUser(String username) {
-        return (userRepository.findByUsername(username.toLowerCase(Locale.ROOT)) != null);
-    }
+    public boolean hasUser(String username);
 }
---
+++ src/main/java/com/rafalcendrowski/AccountApplication/user/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package com.rafalcendrowski.AccountApplication.user;
+
+import com.rafalcendrowski.AccountApplication.payment.Payment;
+import com.rafalcendrowski.AccountApplication.user.User;
+import com.rafalcendrowski.AccountApplication.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserDetailsService, UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username.toLowerCase(Locale.ROOT));
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        } else {
+            return user;
+        }
+    }
+
+    public User loadByUsername(String username) {
+        User user = userRepository.findByUsername(username.toLowerCase(Locale.ROOT));
+        if (user == null) {
+            throw new IllegalArgumentException("User not found");
+        } else {
+            return user;
+        }
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+    }
+
+    public List<User> loadAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public boolean hasUser(String username) {
+        return (userRepository.findByUsername(username.toLowerCase(Locale.ROOT)) != null);
+    }
+}
