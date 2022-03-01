5f12f2cd8858c21cc0a3a0252ff5c079178c821e
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-01 17:31:30 +0100

    split payment service into interface and implementation

--- src/main/java/com/rafalcendrowski/AccountApplication/payment/PaymentService.java
+++ src/main/java/com/rafalcendrowski/AccountApplication/payment/PaymentService.java
@@ -1,41 +1,19 @@
 package com.rafalcendrowski.AccountApplication.payment;
 
 import com.rafalcendrowski.AccountApplication.user.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class PaymentService {
+public interface PaymentService {
 
-    private final PaymentRepository paymentRepository;
+    public Payment savePayment(Payment payment);
 
-    public Payment savePayment(Payment payment) {
-        return paymentRepository.save(payment);
-    }
+    public void deletePayment(Payment payment);
 
-    public void deletePayment(Payment payment) {
-        paymentRepository.delete(payment);
-    }
+    public Payment loadByEmployeeAndPeriod(User user, String period);
 
-    public Payment loadByEmployeeAndPeriod(User user, String period) {
-        Payment payment = paymentRepository.findByEmployeePeriod(user, period);
-        if (payment == null) {
-            throw new IllegalArgumentException("Payment does not exist");
-        } else {
-            return payment;
-        }
-    }
+    public List<Payment> loadByEmployee(User employee);
 
-    public List<Payment> loadByEmployee(User employee) {
-        return Arrays.stream(paymentRepository.findByEmployee(employee)).toList();
-    }
-
-    public boolean hasPayment(User employee, String period) {
-        Payment payment = paymentRepository.findByEmployeePeriod(employee, period);
-        return (payment != null);
-    }
+    public boolean hasPayment(User employee, String period);
 }
