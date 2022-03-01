f60d25a38b47e8c007a7fb168bcc4c3fc07a3bdf
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-01 19:57:28 +0100

    added test save and delete methods

--- src/test/java/com/rafalcendrowski/AccountApplication/user/UserServicelTest.java
+++ src/test/java/com/rafalcendrowski/AccountApplication/user/UserServicelTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,11 +30,16 @@ class UserServicelTest {
 
     @Test
     void saveUser() {
-
+        when(userRepository.save(any(User.class))).then(returnsFirstArg());
+        User user = new User();
+        assertEquals(userService.saveUser(user), user);
     }
 
     @Test
     void deleteUser() {
+        User user = new User();
+        userService.deleteUser(user);
+        verify(userRepository).delete(user);
     }
 
     @Test
