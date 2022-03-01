a8d1d940b43e990ff8a27990b348fe3c2391cc59
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-01 19:35:45 +0100

    proper init for UserServiceTest

--- src/test/java/com/rafalcendrowski/AccountApplication/user/UserServicelTest.java
+++ src/test/java/com/rafalcendrowski/AccountApplication/user/UserServicelTest.java
@@ -1,10 +1,21 @@
 package com.rafalcendrowski.AccountApplication.user;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class UserServicelTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserService userService = new UserServiceImpl();
 
     @Test
     void loadByUsername() {
