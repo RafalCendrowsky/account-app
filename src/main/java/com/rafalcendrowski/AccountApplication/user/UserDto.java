eace0cef71c037864eedd613d712e6784ac5c45d
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-06 18:55:17 +0100

    renamed UserBody to UserRegisterDto and moved to user package

---
+++ src/main/java/com/rafalcendrowski/AccountApplication/user/UserDto.java
@@ -0,0 +1,26 @@
+package com.rafalcendrowski.AccountApplication.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegisterDto {
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String lastname;
+    @NotNull
+    @Email
+    @Pattern(regexp = ".*@acme\\.com")
+    private String email;
+    @NotEmpty
+    @Size(min = 12)
+    @JsonIgnore
+    private String password;
+}
