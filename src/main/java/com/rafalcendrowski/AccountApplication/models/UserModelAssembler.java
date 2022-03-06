c035134a4df1e76e32c261d45992da5d690546a7
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-06 21:59:49 +0100

    created ModelAssembler for User class

--- src/main/java/com/rafalcendrowski/AccountApplication/models/UserModelAssembler.java
+++ src/main/java/com/rafalcendrowski/AccountApplication/models/UserModelAssembler.java
@@ -1,2 +1,22 @@
-package com.rafalcendrowski.AccountApplication.models;public class UserModelAssembler {
+package com.rafalcendrowski.AccountApplication.models;
+
+import com.rafalcendrowski.AccountApplication.controllers.UserController;
+import com.rafalcendrowski.AccountApplication.user.User;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public class UserModelAssembler implements RepresentationModelAssembler<User, EntityModel<User>> {
+
+    @Override
+    public EntityModel<User> toModel(User user) {
+        return EntityModel.of(user,
+                linkTo(methodOn(UserController.class).getUser(user.getUsername())).withSelfRel(),
+                linkTo(methodOn(UserController.class).getUsers()).withRel("users")
+        );
+    }
 }
