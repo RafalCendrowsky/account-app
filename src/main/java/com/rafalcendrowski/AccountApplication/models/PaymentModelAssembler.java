9d509dd22e6e888f69746c493c7b4fcb8a721bc2
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-06 23:41:50 +0100

    added Component annotation

--- src/main/java/com/rafalcendrowski/AccountApplication/models/PaymentModelAssembler.java
+++ src/main/java/com/rafalcendrowski/AccountApplication/models/PaymentModelAssembler.java
@@ -2,13 +2,14 @@ package com.rafalcendrowski.AccountApplication.models;
 
 import com.rafalcendrowski.AccountApplication.controllers.PaymentsController;
 import com.rafalcendrowski.AccountApplication.payment.Payment;
-import com.rafalcendrowski.AccountApplication.payment.PaymentDto;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@Component
 public class PaymentModelAssembler implements RepresentationModelAssembler<Payment, EntityModel<Payment>> {
     @Override
     public EntityModel<Payment> toModel(Payment payment) {
