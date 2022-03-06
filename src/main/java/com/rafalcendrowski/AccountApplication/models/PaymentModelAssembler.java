358af7b5cead99d01e8cd848df076b5cfd277668
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-06 23:54:33 +0100

    changed to return PaymentDto EntityModel, added link to employee User

--- src/main/java/com/rafalcendrowski/AccountApplication/models/PaymentModelAssembler.java
+++ src/main/java/com/rafalcendrowski/AccountApplication/models/PaymentModelAssembler.java
@@ -1,7 +1,9 @@
 package com.rafalcendrowski.AccountApplication.models;
 
 import com.rafalcendrowski.AccountApplication.controllers.PaymentsController;
+import com.rafalcendrowski.AccountApplication.controllers.UserController;
 import com.rafalcendrowski.AccountApplication.payment.Payment;
+import com.rafalcendrowski.AccountApplication.payment.PaymentDto;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
@@ -10,11 +12,12 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class PaymentModelAssembler implements RepresentationModelAssembler<Payment, EntityModel<Payment>> {
+public class PaymentModelAssembler implements RepresentationModelAssembler<Payment, EntityModel<PaymentDto>> {
     @Override
-    public EntityModel<Payment> toModel(Payment payment) {
-        return EntityModel.of(payment,
+    public EntityModel<PaymentDto> toModel(Payment payment) {
+        return EntityModel.of(PaymentDto.of(payment),
                 linkTo(methodOn(PaymentsController.class).getPayment(payment.getId())).withSelfRel(),
+                linkTo(methodOn(UserController.class).getUser(payment.getEmployee().getId())).withRel("user"),
                 linkTo(methodOn(PaymentsController.class).getPayments()).withRel("payments")
         );
     }
