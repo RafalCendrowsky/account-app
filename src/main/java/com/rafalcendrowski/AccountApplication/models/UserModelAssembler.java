7a32c2031eca36bfc734276ac49b3bb6f3f511c3
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-06 22:07:05 +0100

    added controller annotation

--- src/main/java/com/rafalcendrowski/AccountApplication/models/UserModelAssembler.java
+++ src/main/java/com/rafalcendrowski/AccountApplication/models/UserModelAssembler.java
@@ -7,10 +7,12 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@Component
 public class UserModelAssembler implements RepresentationModelAssembler<User, EntityModel<UserDto>> {
 
     @Override
