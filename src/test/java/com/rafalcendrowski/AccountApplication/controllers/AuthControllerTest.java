579f7bd4b2afc18db36964e7a53353e32a973f9c
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-06 14:44:29 +0100

    created test class for auth controller

--- src/test/java/com/rafalcendrowski/AccountApplication/controllers/AuthControllerTest.java
+++ src/test/java/com/rafalcendrowski/AccountApplication/controllers/AuthControllerTest.java
@@ -1,6 +1,23 @@
 package com.rafalcendrowski.AccountApplication.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rafalcendrowski.AccountApplication.user.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = AuthController.class)
 class AuthControllerTest {
-  
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    UserService userService;
+
 }
