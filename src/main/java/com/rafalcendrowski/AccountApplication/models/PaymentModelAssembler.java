58a7a982ce7e873ceb05140fde8e8e1c76a6ddba
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-06 23:31:18 +0100

    finalized toModel function for PaymentModelAssembler

--- src/main/java/com/rafalcendrowski/AccountApplication/models/PaymentModelAssembler.java
+++ src/main/java/com/rafalcendrowski/AccountApplication/models/PaymentModelAssembler.java
@@ -1,13 +1,20 @@
 package com.rafalcendrowski.AccountApplication.models;
 
+import com.rafalcendrowski.AccountApplication.controllers.PaymentsController;
 import com.rafalcendrowski.AccountApplication.payment.Payment;
 import com.rafalcendrowski.AccountApplication.payment.PaymentDto;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 
-public class PaymentModelAssembler implements RepresentationModelAssembler<Payment, EntityModel<PaymentDto>> {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public class PaymentModelAssembler implements RepresentationModelAssembler<Payment, EntityModel<Payment>> {
     @Override
-    public EntityModel<PaymentDto> toModel(Payment entity) {
-        return null;
+    public EntityModel<Payment> toModel(Payment payment) {
+        return EntityModel.of(payment,
+                linkTo(methodOn(PaymentsController.class).getPayment(payment.getId())).withSelfRel(),
+                linkTo(methodOn(PaymentsController.class).getPayments()).withRel("payments")
+        );
     }
 }
