1869dcfdc932a9848bacbd950014b8719cb2607e
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-07 20:19:21 +0100

    changed return values of functions to hateoas entities

--- src/main/java/com/rafalcendrowski/AccountApplication/controllers/EmployeeController.java
+++ src/main/java/com/rafalcendrowski/AccountApplication/controllers/EmployeeController.java
@@ -1,12 +1,13 @@
 package com.rafalcendrowski.AccountApplication.controllers;
 
+import com.rafalcendrowski.AccountApplication.models.EmployeePaymentModelAssembler;
 import com.rafalcendrowski.AccountApplication.payment.Payment;
-import com.rafalcendrowski.AccountApplication.payment.PaymentRepository;
+import com.rafalcendrowski.AccountApplication.payment.PaymentDto;
 import com.rafalcendrowski.AccountApplication.payment.PaymentService;
 import com.rafalcendrowski.AccountApplication.user.User;
-import com.rafalcendrowski.AccountApplication.user.UserRepository;
-import com.rafalcendrowski.AccountApplication.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,40 +20,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/empl")
 public class EmployeeController {
 
     @Autowired
-    UserService userService;
-
-    @Autowired
     PaymentService paymentService;
 
+    @Autowired
+    EmployeePaymentModelAssembler employeePaymentModelAssembler;
+
     @GetMapping(value = "/payment", params = {"period"})
-    public Map<String, Object> getPayment(@RequestParam String period,
-                                          @AuthenticationPrincipal User user) {
+    public EntityModel<PaymentDto> getPayment(@RequestParam String period,
+                                              @AuthenticationPrincipal User user) {
         if (!period.matches("(0[1-9]|1[0-2])-\\d\\d\\d\\d")) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid period");
         }
         Payment payment = paymentService.loadByEmployeeAndPeriod(user, period);
-        return getPaymentMap(user, payment);
-    }
-
-    private Map<String, Object> getPaymentMap(User user, Payment payment) {
-        String dollars = String.valueOf(payment.getSalary()/100L);
-        dollars = dollars.isBlank() ? "0 dollar(s)" : dollars.concat(" dollar(s) ");
-        String cents = String.valueOf(payment.getSalary()%100L);
-        cents = cents.isBlank() ? "0 cent(s)" : cents.concat(" cent(s)");
-        return Map.of("name", user.getName(), "lastname", user.getLastname(),
-                "period", payment.getPeriod(), "salary", dollars + cents);
+        return employeePaymentModelAssembler.toModel(payment);
     }
 
     @GetMapping("/payment")
-    public List<Map<String, Object>> getPayment(@AuthenticationPrincipal User user) {
-        List<Map<String, Object>> payments = new ArrayList<>();
-        paymentService.loadByEmployee(user).forEach(
-                payment -> payments.add(getPaymentMap(user, payment)));
-        return payments;
+    public CollectionModel<EntityModel<PaymentDto>> getPayments(@AuthenticationPrincipal User user) {
+        List<EntityModel<PaymentDto>> payments = new ArrayList<>();
+        paymentService.loadByEmployee(user).stream()
+                .map(employeePaymentModelAssembler::toModel).toList();
+        return CollectionModel.of(payments,
+                linkTo(methodOn(EmployeeController.class).getPayments(null)).withSelfRel());
     }
 }
