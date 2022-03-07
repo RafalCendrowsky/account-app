1f11dd99174bad26c96575c6340b78b43a9a8ff0
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2022-03-07 20:25:04 +0100

    created assembler for users in api/empl endpoint

--- src/main/java/com/rafalcendrowski/AccountApplication/models/EmployeeModelAssembler.java
+++ src/main/java/com/rafalcendrowski/AccountApplication/models/EmployeeModelAssembler.java
@@ -1,2 +1,24 @@
-package com.rafalcendrowski.AccountApplication.models;public class EmployeeModelAssembler {
+package com.rafalcendrowski.AccountApplication.models;
+
+import com.rafalcendrowski.AccountApplication.controllers.EmployeeController;
+import com.rafalcendrowski.AccountApplication.controllers.UserController;
+import com.rafalcendrowski.AccountApplication.user.User;
+import com.rafalcendrowski.AccountApplication.user.UserDto;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<User, EntityModel<UserDto>> {
+
+    @Override
+    public EntityModel<UserDto> toModel(User user) {
+        return EntityModel.of(UserDto.of(user),
+                linkTo(methodOn(EmployeeController.class).getEmployee(null)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).getPayments(null)).withRel("payments")
+        );
+    }
 }
